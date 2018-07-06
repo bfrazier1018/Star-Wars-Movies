bd130a83f8cdea43f7efceffe3ea1fc560b4ba45
bfrazier1018 <bfrazier1018@gmail.com>
2018-07-06 17:12:45 -0400

    Add anchor Links and Not Found Page

--- routes/index.js
+++ routes/index.js
@@ -13,8 +13,28 @@ exports.home = (req, res) => {
 
 // Movie Single Page
 exports.movie_single = (req, res) => {
+
 	var episode_number = req.params.episode_number;
-	res.send("This is the page for episode " + episode_number);
+	var movies = moviesJSON.movies;
+
+ 	if(episode_number >= 1 && episode_number <= 6) {
+ 		
+ 		var movie = movies[episode_number - 1];
+		var title = movie.title;
+		var main_characters = movie.main_characters;
+
+		res.render('movie_single', {
+			movies : movies, 
+			title : title,
+			movie : movie,
+			main_characters : main_characters
+		});
+ 	} else {
+ 		res.render('notFound', {
+ 			movies : movies,
+ 			title : "This is Not the Page Your Are Looking For"
+ 		});
+ 	}
 };
 
 // Not Found Routes
